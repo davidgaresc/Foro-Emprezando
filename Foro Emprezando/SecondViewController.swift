d833069fbabf4f5983e4c2d92ffd108caa4afd62
David Antonio Gracia Escobar <davidgaresc87@hotmail.com>
2015-10-22 09:56:30 -0500

    Initial Commit

---
+++ Foro Emprezando/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Foro Emprezando
+//
+//  Created by cCeo on 22/10/15.
+//  Copyright (c) 2015 CCEO. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
