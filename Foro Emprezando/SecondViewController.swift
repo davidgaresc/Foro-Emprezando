aaa5626f706ed71a8a92a315411b3db42e3da6a3
David Antonio Gracia Escobar <davidgaresc87@hotmail.com>
2015-10-29 10:38:58 -0600

    Actualizacion de datos e imagenes

--- Foro Emprezando/SecondViewController.swift
+++ Foro Emprezando/SecondViewController.swift
@@ -13,16 +13,16 @@ class SecondViewController: UIViewController, UICollectionViewDataSource, UIColl
     @IBOutlet weak var collectionView: UICollectionView!
     
     let expositorName = ["Saúl Haro Vázquez", "Jorge García", "Juan José Díaz", "Francisco García Sánchez", "Sergio Ramírez Zúñiga", "Gaudalupe Gómez", "Arturo Gilio Hamdan", "Vidal Cantú"]
-    let description1 = "Macrolynk \nGanador del emprendedor del año por la revista CNN Expansión 2015, y consultor de empresas como LG,GE,PLEXUS,MAHLE entre otras."
-    let description2 = "Outcom \nDirector de empresa con representación en México, Co-fundador de la Alianza internacional de Empresarios Jóvenes del G20."
-    let description3 = "Eudoxa \nEmpresario filosofo que diseño el plan estratégico de la empresa de transportes ADO y Coparmex Nacional."
-    let description4 = "Pacomer y Coachildren \nEmpresario de 12 años con dos empresas. Una especializada en asesorar niños en la creación de empresas y la segunda de servicios de catering."
-    let description5 = "Academia de Baloncesto Indígena de México. \nEntrenador del equipo de niños triqui, ganador de la copa mundial de mini baloncesto en Argentina."
-    let description6 = "Remedios Mágicos \nAlquimista en jefe de la botica de dulces más importante de todo México. Y ganadora de “El Monstruo de la mercadotecnia” por CNN Expansión."
-    let description7 = "Palacio del Centenario \nMatador de toros y empresario proveedor de la empresa Nestle y socio mayoritario del centro de espectáculos coliseo."
-    let description8 = "Kenio Flims y veramiko \nProductor de la película Cantinflas y Co-productor de la película “The Perfect Game. Ganador del emprendedor de año CNN Expansión en el 2013 con la empresa Veramiko."
-    let descriptionTextView = [description1, description2, description3, description4, description5, description6, description7, description8]
-    let imageArray = []
+    let descripcion = "Macrolynk \nGanador del emprendedor del año por la revista CNN Expansión 2015, y consultor de empresas como LG,GE,PLEXUS,MAHLE entre otras."
+    let descripcion2 = "Outcom \nDirector de empresa con representación en México, Co-fundador de la Alianza internacional de Empresarios Jóvenes del G20."
+    let descripcion3 = "Eudoxa \nEmpresario filosofo que diseño el plan estratégico de la empresa de transportes ADO y Coparmex Nacional."
+    let descirpcion4 = "Pacomer y Coachildren \nEmpresario de 12 años con dos empresas. Una especializada en asesorar niños en la creación de empresas y la segunda de servicios de catering."
+    let descirpcion5 = "Academia de Baloncesto Indígena de México. \nEntrenador del equipo de niños triqui, ganador de la copa mundial de mini baloncesto en Argentina."
+    let descripcion6 = "Remedios Mágicos \nAlquimista en jefe de la botica de dulces más importante de todo México. Y ganadora de “El Monstruo de la mercadotecnia” por CNN Expansión."
+    let descripcion7 = "Palacio del Centenario \nMatador de toros y empresario proveedor de la empresa Nestle y socio mayoritario del centro de espectáculos coliseo."
+    let descripcion8 = "Kenio Flims y veramiko \nProductor de la película Cantinflas y Co-productor de la película “The Perfect Game. Ganador del emprendedor de año CNN Expansión en el 2013 con la empresa Veramiko."
+    //let descriptionTextView = ["\(descripcion)", "\(descripcion2)", "\(descripcion3)", "\(descirpcion4)", "\(descirpcion5)", "\(descripcion6)", "\(descripcion7)", "\(descripcion8)"]
+    let imagePrograma = [UIImage(named: "imagen1"), UIImage(named: "imagen2"), UIImage(named: "imagen3"), UIImage(named: "imagen4"), UIImage(named: "imagen5"), UIImage(named: "imagen6"), UIImage(named: "imagen7"), UIImage(named: "imagen8"), UIImage(named: "imagen9")]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,7 +33,26 @@ class SecondViewController: UIViewController, UICollectionViewDataSource, UIColl
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return self.expositorName.count
+        
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as! CollectionViewCell
+        
+        cell.imageViewPrograma?.image = self.imagePrograma[indexPath.row]
+        cell.ProgramaLabel?.text = self.expositorName[indexPath.row]
+        //cell.prgramaTextView?.attributedText = self.descriptionTextView[indexPath.row]
+        
+        
+        return cell
+    }
 
 
 }
+
+//"Macrolynk \nGanador del emprendedor del año por la revista CNN Expansión 2015, y consultor de empresas como LG,GE,PLEXUS,MAHLE entre otras.", "Outcom \nDirector de empresa con representación en México, Co-fundador de la Alianza internacional de Empresarios Jóvenes del G20.", "Eudoxa \nEmpresario filosofo que diseño el plan estratégico de la empresa de transportes ADO y Coparmex Nacional.", "Pacomer y Coachildren \nEmpresario de 12 años con dos empresas. Una especializada en asesorar niños en la creación de empresas y la segunda de servicios de catering.", "Academia de Baloncesto Indígena de México. \nEntrenador del equipo de niños triqui, ganador de la copa mundial de mini baloncesto en Argentina.", "Remedios Mágicos \nAlquimista en jefe de la botica de dulces más importante de todo México. Y ganadora de “El Monstruo de la mercadotecnia” por CNN Expansión.", "Palacio del Centenario \nMatador de toros y empresario proveedor de la empresa Nestle y socio mayoritario del centro de espectáculos coliseo.", "Kenio Flims y veramiko \nProductor de la película Cantinflas y Co-productor de la película “The Perfect Game. Ganador del emprendedor de año CNN Expansión en el 2013 con la empresa Veramiko."
 
